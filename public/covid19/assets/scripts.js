7ccfee4adfdfe209a2c21e17ec1805fc5b839b13
Julio Palma Quezada <j.palma.quezada@gmail.com>
2022-01-14 20:04:27 -0300

    Modificacion de grafico

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -1,21 +1,47 @@
-(async function() {
-    const data = await fetch("/api/total")
-    const covid = await data.json();
-    drawChart(covid);
-    drawTable(covid);
+$("#form-login").on("submit", async function(ev) {
+    ev.preventDefault();
 
+    const email = $("#exampleInputEmail1").val();
+    const password = $("#exampleInputPassword1").val();
+    console.log(email);
+    const data = await fetch("/api/login", {
 
+        method: 'POST',
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
 
-})()
+    });
 
-$('#verdetalle').on('click', function() {
+    const jwt = await data.json()
+    const token = jwt.token;
+    localStorage.setItem("token", token);
 
-    alert("Llegué");
-
-    $("#exampleModal .modal-body").html("<h1>Hola, soy un modal</h1>");
-    $("#exampleModal").modal("show");
+    init(token);
 
 });
+
+async function init(token) {
+    const data = await fetch("/api/total")
+    const covid = await data.json();
+
+    drawTable(covid.data);
+    drawChart(covid);
+
+    $("#form-login").addClass("d-none").removeClass("d-block");
+    $(".table").addClass("d-block").removeClass("d-none");
+    $("#idGrafico").addClass("d-block").removeClass("d-none");
+    $("#salir").addClass("d-block").removeClass("d-none");
+
+}
+
+(async function() {
+    const token = localStorage.getItem("token");
+    if (token) {
+        init(token);
+    }
+})()
 
 let filterArrayCovid;
 
@@ -88,6 +114,92 @@ function drawChart(covid) {
 
 }
 
-function drawTable(covid) {
-    console.log("Llegué a dibujar la tabla")
+const ArrayPaises = [];
+
+function drawTable(datos) {
+    let i = 0;
+    for (const dato of datos) {
+        if (dato.deaths >= 100000) {
+            $('.table').append(`<tr>
+                <th scope="row">${i}</th>
+                <td>${dato.location}</td>
+                <td>${dato.confirmed}</td>
+                <td>${dato.deaths}</td>
+                <td>${dato.recovered}</td>
+                <td>${dato.active}</td>
+                <td><button onclick="GraficoModal(${i})" class="btn btn-primary"> ver detalle </button></td>
+                </tr>
+        `)
+            ArrayPaises.push(dato.location);
+            i++
+        }
+    }
 }
+
+async function GraficoModal(ind) {
+    const data = await fetch(`/api/countries/${ArrayPaises[ind]}`);
+    const data2 = await data.json();
+
+    console.log(data2.data);
+
+    $("#exampleModal").modal("show");
+
+
+    let chart = new CanvasJS.Chart("chartContainerModal", {
+        animationEnabled: true,
+        title: {
+            text: `País con covid 19:${ArrayPaises[ind]}`
+        },
+        axisY: {
+            title: "Casos de covid",
+            titleFontColor: "#4F81BC",
+            lineColor: "#4F81BC",
+            labelFontColor: "#4F81BC",
+            tickColor: "#4F81BC"
+        },
+        toolTip: {
+            shared: true
+        },
+        legend: {
+            cursor: "pointer",
+            itemclick: toggleDataSeries
+        },
+        data: [{
+            type: "column",
+            name: "confirmados",
+            legendText: "Numeros de casos",
+            showInLegend: true,
+            dataPoints: [
+                { label: "confirmados", y: data2.data.confirmed },
+                { label: "Muertos", y: data2.data.deaths },
+                { label: "recuperados", y: data2.data.recovered },
+                { label: "activos", y: data2.data.active },
+            ]
+        }]
+    });
+
+    chart.render();
+
+    function toggleDataSeries(e) {
+        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+        } else {
+            e.dataSeries.visible = true;
+        }
+        chart.render();
+    }
+
+}
+
+$('#salir').on('click', async function(ev) {
+    ev.preventDefault();
+    $('#exampleInputEmail1').val("");
+    $('#exampleInputPassword1').val("");
+
+    $("#form-login").addClass("d-block").removeClass("d-none");
+    $(".table").addClass("d-none").removeClass("d-block");
+    $("#idGrafico").addClass("d-none").removeClass("d-block");
+    $("#salir").addClass("d-none").removeClass("d-block");
+    localStorage.removeItem("token");
+
+});
