73aee8ea11916eb90bbb26d86ff0e3f3a72448b5
Julio Palma Quezada <j.palma.quezada@gmail.com>
2022-01-15 12:31:36 -0300

    Con cambios en el formulario y los graficos

--- public/CasosHito2/assets/js/script.js
+++ public/CasosHito2/assets/js/script.js
@@ -3,7 +3,7 @@ $("#form-login").on("submit", async function(ev) {
 
     const email = $("#exampleInputEmail1").val();
     const password = $("#exampleInputPassword1").val();
-
+    console.log(email);
     const data = await fetch("/api/login", {
 
         method: 'POST',
@@ -20,20 +20,19 @@ $("#form-login").on("submit", async function(ev) {
 
     init(token);
 
-    console.log(token);
 });
-
 
 async function init(token) {
     const data = await fetch("/api/total")
     const covid = await data.json();
 
-    // drawTable(covid.data);
-    // drawChart(covid);
+    drawTable(covid.data);
+    drawChart(covid);
 
     $("#form-login").addClass("d-none").removeClass("d-block");
-    // $("#idGrafico").addClass("d-block").removeClass("d-none");
-    // $("#salir").addClass("d-block").removeClass("d-none");
+    $(".table").addClass("d-block").removeClass("d-none");
+    $("#idGrafico").addClass("d-block").removeClass("d-none");
+    $("#navHito").addClass("d-block").removeClass("d-none");
 
 }
 
@@ -43,3 +42,185 @@ async function init(token) {
         init(token);
     }
 })()
+
+let filterArrayCovid;
+
+function drawChart(covid) {
+
+    filterArrayCovid = covid.data.filter(pais => pais.deaths > 100000);
+
+    let covidConfirmados = new Array();
+    let covidMuertos = new Array();
+
+    for (pais of filterArrayCovid) {
+        covidConfirmados.push({ label: pais.location, y: pais.confirmed });
+        covidMuertos.push({ label: pais.location, y: pais.deaths });
+    }
+
+    let chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        title: {
+            text: "Paises con Covid19"
+        },
+        axisY: {
+            title: "Casos de covid Confirmados",
+            titleFontColor: "#4F81BC",
+            lineColor: "#4F81BC",
+            labelFontColor: "#4F81BC",
+            tickColor: "#4F81BC"
+        },
+        axisY2: {
+            title: "Muertos por Covid",
+            titleFontColor: "#C0504E",
+            lineColor: "#C0504E",
+            labelFontColor: "#C0504E",
+            tickColor: "#C0504E"
+        },
+        toolTip: {
+            shared: true
+        },
+        legend: {
+            cursor: "pointer",
+            itemclick: toggleDataSeries
+        },
+        data: [{
+                type: "column",
+                name: "confirmados",
+                legendText: "confirmados",
+                showInLegend: true,
+                dataPoints: covidConfirmados
+            },
+            {
+                type: "column",
+                name: "muertos",
+                legendText: "muertos",
+                axisYType: "secondary",
+                showInLegend: true,
+                dataPoints: covidMuertos
+            }
+        ]
+    });
+
+    chart.render();
+
+    function toggleDataSeries(e) {
+        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+        } else {
+            e.dataSeries.visible = true;
+        }
+        chart.render();
+    }
+
+}
+
+const ArrayPaises = [];
+
+function drawTable(datos) {
+    let i = 0;
+    for (const dato of datos) {
+        if (dato.deaths >= 100000) {
+            $('.table').append(`<tr>
+                <th scope="row">${i}</th>
+                <td>${dato.location}</td>
+                <td>${dato.confirmed}</td>
+                <td>${dato.deaths}</td>
+                <td>${dato.recovered}</td>
+                <td>${dato.active}</td>
+                <td><button onclick="GraficoModal(${i})" class="btn btn-primary"> ver detalle </button></td>
+                </tr>
+        `)
+            ArrayPaises.push(dato.location);
+            i++
+        }
+    }
+}
+
+async function GraficoModal(ind) {
+    const data = await fetch(`/api/countries/${ArrayPaises[ind]}`);
+    const data2 = await data.json();
+
+    console.log(data2.data);
+
+    $("#exampleModal").modal("show");
+
+
+    let chart = new CanvasJS.Chart("chartContainerModal", {
+        animationEnabled: true,
+        title: {
+            text: `País con covid 19:${ArrayPaises[ind]}`
+        },
+        axisY: {
+            title: "Casos de covid",
+            titleFontColor: "#4F81BC",
+            lineColor: "#4F81BC",
+            labelFontColor: "#4F81BC",
+            tickColor: "#4F81BC"
+        },
+        toolTip: {
+            shared: true
+        },
+        legend: {
+            cursor: "pointer",
+            itemclick: toggleDataSeries
+        },
+        data: [{
+            type: "column",
+            name: "confirmados",
+            legendText: "Numeros de casos",
+            showInLegend: true,
+            dataPoints: [
+                { label: "confirmados", y: data2.data.confirmed },
+                { label: "Muertos", y: data2.data.deaths },
+                { label: "recuperados", y: data2.data.recovered },
+                { label: "activos", y: data2.data.active },
+            ]
+        }]
+    });
+
+    chart.render();
+
+    function toggleDataSeries(e) {
+        if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+        } else {
+            e.dataSeries.visible = true;
+        }
+        chart.render();
+    }
+
+}
+
+$('#cerrarSesion').on('click', async function(ev) {
+    ev.preventDefault();
+    $('#exampleInputEmail1').val("");
+    $('#exampleInputPassword1').val("");
+
+    $("#form-login").addClass("d-block").removeClass("d-none");
+    $(".table").addClass("d-none").removeClass("d-block");
+    $("#idGrafico").addClass("d-none").removeClass("d-block");
+    $("#navHito").addClass("d-none").removeClass("d-block");
+    localStorage.removeItem("token");
+
+});
+
+$('#home').on('click', async function(ev) {
+    ev.preventDefault();
+
+    $(".table").addClass("d-block").removeClass("d-none");
+    $("#idGrafico").addClass("d-block").removeClass("d-none");
+
+    alert("Estoy en home")
+
+});
+
+
+$('#situacionChile').on('click', async function(ev) {
+    ev.preventDefault();
+
+    $(".table").addClass("d-none").removeClass("d-block");
+    $("#idGrafico").addClass("d-none").removeClass("d-block");
+
+    alert("Estoy en situacion de chile");
+
+});
